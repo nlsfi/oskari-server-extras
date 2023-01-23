65d24d7fe26fece9e3546e8a46ee55eb81aeece6
Sami Mäkinen <sami.makinen@nls.fi>
2023-01-23 16:43:22 +0200

    Allow passing apikey to requests

--- service-terrain-profile/src/main/java/fi/nls/oskari/terrainprofile/CommandGetCoverage.java
+++ service-terrain-profile/src/main/java/fi/nls/oskari/terrainprofile/CommandGetCoverage.java
@@ -3,6 +3,7 @@ package fi.nls.oskari.terrainprofile;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.util.concurrent.TimeoutException;
+import java.util.function.Supplier;
 
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
@@ -10,6 +11,7 @@ import com.netflix.hystrix.HystrixCommandKey;
 import com.netflix.hystrix.HystrixCommandProperties;
 import com.netflix.hystrix.HystrixThreadPoolProperties;
 
+import fi.nls.oskari.service.ServiceRuntimeException;
 import fi.nls.oskari.util.IOHelper;
 import fi.nls.oskari.util.PropertyUtil;
 
@@ -27,9 +29,18 @@ public class CommandGetCoverage extends HystrixCommand<byte[]> {
     private static final int MAX_RETRIES = 5;
     private static final int SLEEP_BETWEEN_RETRY_MS = 100;
 
-    private final String request;
+    private final Supplier<HttpURLConnection> connectionSupplier;
 
     public CommandGetCoverage(String request) {
+        this(() -> {
+            try {
+                return IOHelper.getConnection(request);
+            } catch (IOException e) {
+                throw new ServiceRuntimeException("Error connecting to service", e);
+            }
+        });
+    }
+    public CommandGetCoverage(Supplier<HttpURLConnection> connectionSupplier) {
         super(Setter
                 .withGroupKey(HystrixCommandGroupKey.Factory.asKey(GROUP_KEY))
                 .andCommandKey(HystrixCommandKey.Factory.asKey(COMMAND_NAME))
@@ -44,12 +55,23 @@ public class CommandGetCoverage extends HystrixCommand<byte[]> {
                         .withCircuitBreakerRequestVolumeThreshold(PropertyUtil.getOptional(GROUP_KEY + ".failrequests", 10))
                         .withCircuitBreakerSleepWindowInMilliseconds(PropertyUtil.getOptional(GROUP_KEY + ".sleepwindow", 10000)))
                 );
-        this.request = request;
+        this.connectionSupplier = connectionSupplier;
+    }
+
+    private HttpURLConnection getConnection() throws IOException {
+        try {
+            return connectionSupplier.get();
+        } catch (ServiceRuntimeException e) {
+            if (e.getCause() instanceof IOException) {
+                throw (IOException) e.getCause();
+            }
+            throw e;
+        }
     }
 
     @Override
     protected byte[] run() throws TimeoutException, IOException, Exception {
-        HttpURLConnection conn = IOHelper.getConnection(request);
+        HttpURLConnection conn = getConnection();
         int sc = conn.getResponseCode();
 
         int tryCounter = 0;
@@ -58,7 +80,7 @@ public class CommandGetCoverage extends HystrixCommand<byte[]> {
                 throw new TimeoutException();
             }
             Thread.sleep(SLEEP_BETWEEN_RETRY_MS);
-            conn = IOHelper.getConnection(request);
+            conn = getConnection();
             sc = conn.getResponseCode();
         }
 
